bab5a9f5ab7a1d8b68e8b88a2eb57d1e641b6082
Michael McCune <msm@redhat.com>
2017-03-23 16:16:40 -0400

    add some sparkjava stuff

--- src/main/java/com/mycompany/app/JavaSparkPi.java
+++ src/main/java/com/mycompany/app/JavaSparkPi.java
@@ -18,6 +18,7 @@ import org.apache.spark.api.java.function.Function2;
 
 import java.util.ArrayList;
 import java.util.List;
+import static spark.Spark.*;
 
 /**
  * Computes an approximation to pi
@@ -25,35 +26,38 @@ import java.util.List;
  */
 public final class JavaSparkPi {
 
-  public static void main(String[] args) throws Exception {
-    SparkConf sparkConf = new SparkConf().setAppName("JavaSparkPi");
-    JavaSparkContext jsc = new JavaSparkContext(sparkConf);
+    public static void main(String[] args) throws Exception {
+        SparkConf sparkConf = new SparkConf().setAppName("JavaSparkPi");
+        JavaSparkContext jsc = new JavaSparkContext(sparkConf);
 
-    int slices = (args.length == 1) ? Integer.parseInt(args[0]) : 2;
-    int n = 100000 * slices;
-    List<Integer> l = new ArrayList<Integer>(n);
-    for (int i = 0; i < n; i++) {
-      l.add(i);
+        int slices = (args.length == 1) ? Integer.parseInt(args[0]) : 2;
+        int n = 100000 * slices;
+        List<Integer> l = new ArrayList<Integer>(n);
+        for (int i = 0; i < n; i++) {
+            l.add(i);
+        }
+
+        JavaRDD<Integer> dataSet = jsc.parallelize(l, slices);
+
+        int count = dataSet.map(new Function<Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer) {
+                double x = Math.random() * 2 - 1;
+                double y = Math.random() * 2 - 1;
+                return (x * x + y * y < 1) ? 1 : 0;
+            }
+        }).reduce(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer, Integer integer2) {
+                return integer + integer2;
+            }
+        });
+
+        String pival = "Pi is rouuuughly " + 4.0 * count / n;
+
+        jsc.stop();
+
+        port(8080);
+        get("/", (req, res) -> pival);
     }
-
-    JavaRDD<Integer> dataSet = jsc.parallelize(l, slices);
-
-    int count = dataSet.map(new Function<Integer, Integer>() {
-      @Override
-      public Integer call(Integer integer) {
-        double x = Math.random() * 2 - 1;
-        double y = Math.random() * 2 - 1;
-        return (x * x + y * y < 1) ? 1 : 0;
-      }
-    }).reduce(new Function2<Integer, Integer, Integer>() {
-      @Override
-      public Integer call(Integer integer, Integer integer2) {
-        return integer + integer2;
-      }
-    });
-
-    System.out.println("Pi is rouuuughly " + 4.0 * count / n);
-
-    jsc.stop();
-  }
 }
